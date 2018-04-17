970de480f3d32a924e49b28574813250210a800d
louliying <louliying@chexiang.com>
2018-04-18 00:03:49 +0800

    增加一些常 util

--- config/app.js
+++ config/app.js
@@ -5,33 +5,8 @@
 
 var ENV = "prod";
 
-// 静态图片url domain
-var imgMap = {
-  'prod': 'cximg',
-  'pre': 'chexiangpre',
-  'sit': 'chexiangsit'
-};
-// ajaxurl domain
-var domainMap = {
-  'prod': 'chexiang',
-  'pre': 'chexiangpre',
-  'sit': 'chexiangsit'
-};
-// 支付 templateId
-var payTemplateIdMap = {
-  'prod': 'nItsmIDSGRdzmVFunWRr6CIcehX2uWcr-OasykliWyg',
-  'pre': 'YxfMshipx_ZPk8SLALr4Fp9JUvTnyrBaBUKXFm4g2Ow',
-  'sit': '-1wqNhiS3y7p7hpA0t02Xgtlure0qqIp5E1uMsx3ba4'
-};
-// 预约 templateId
-var remindTemplateIdMap = {
-  'prod': 'LAxC4VoD-O58qk36ukbY4pF70KFiwxG8ysFkiFCGU4c',
-  'pre': 'ttfuHxBejLP3JS_Q8KgPcq_9dO0zzDu-7VJ837tJskw',
-  'sit': 'xXtXQaHgEm5M48u9i1ouQd9uqwgXDJlBA7St7OluFjc'
-};
-
-var domain = domainMap[ENV];
-var imgUrl = imgMap[ENV];
+var domain = 'sit';
+var imgUrl = '';
 var host = "";
 
 var config = {
@@ -39,16 +14,6 @@ var config = {
     domain,
     host,
     debug: false,
-    // 消息模板Id
-    templateId: {
-      pay: payTemplateIdMap[ENV],
-      remind: remindTemplateIdMap[ENV]
-    },
-    // 消息模板点击详情去的页面
-    toPage:{
-        pay: 'page/coupon/ucenter/ucenter',
-        remind: 'page/coupon/product/product?mdseCode='
-    },
     //
     // 是否登陆微信
     isWxLoginUrl: `https://mgo.${domain}.com/wxAppLogin/isWxLogin`,
