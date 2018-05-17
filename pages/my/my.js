b18181ca27a893d92e6fd6d22a535b0dd90cc02d
louliying <louliying@chexiang.com>
2018-05-17 10:54:13 +0800

    个人中心首页 ， 调试接口完成

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,15 +1,31 @@
-import { config } from '../../config/api.js';
+import Config from '../../config/api.js';
+import Util from '../../utils/util';
 Page({
     data: {
       authorInfo:{
 
-      }
+      },
+      myData: {}
     },
     onLoad: function(option) {
+      let _this = this;
       this.setData({
         authorInfo: getApp().globalData.userInfo
       });
-      console.log(getApp().globalData.userInfo);
+      // console.log(getApp().globalData.userInfo);
+      let url = Config.my,
+          token = wx.getStorageSync('token');
+      Util.request(url, {token: token}, 'POST').then((res) => {
+        if (res.code === 200) {
+          //
+          _this.setData({
+            myData: res.data
+          });
+          console.log('33333333333333:', res.data);
+        }
+      }, (err) => {
+
+      });
 
     },
     onShow: function(options) {
--- utils/util.js
+++ utils/util.js
@@ -1,20 +1,4 @@
 import Promise from './bluebird';
-
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
 var Util = {};
 /**
  * 封封微信的的request
@@ -106,7 +90,4 @@ Util.objectToString = function(obj) {
     }
 }
 
-module.exports = {
-  formatTime: formatTime,
-  Util: Util
-}
+module.exports = Util;
